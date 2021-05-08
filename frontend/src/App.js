81b86c3116c37b4ee0ca7725ef26720489a5af31
Viktor Mishyn <viktormishyn@gmail.com>
2021-05-08 23:51:41 +0300

    Create Posts.js, PostLoading.js; Build App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Posts from "./components/Posts";
+import PostLoadingComponent from "./components/PostLoading";
 
-class connectionExample extends React.Component {
-  componentDidMount() {
-    const apiUrl = "http://127.0.0.1:8000/api/";
+function App() {
+  const PostLoading = PostLoadingComponent(Posts);
+
+  const [appState, setAppState] = useState({
+    loading: false,
+    posts: null,
+  });
+
+  useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = `http://127.0.0.1:8000/api/`;
     fetch(apiUrl)
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  }
-  render() {
-    return <div>Example connection</div>;
-  }
+      .then((data) => data.json())
+      .then((posts) => {
+        setAppState({ loading: false, posts: posts });
+      });
+  }, [setAppState]);
+
+  return (
+    <div className="App">
+      <h1>Latest Posts</h1>
+      <PostLoading isLoading={appState.loading} posts={appState.posts} />
+    </div>
+  );
 }
 
-export default connectionExample;
+export default App;
