3552118e0b71742497aa1b4008fdd3a5044422dc
Viktor Mishyn <viktormishyn@gmail.com>
2021-05-08 11:59:30 +0300

    Fix urls

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls', namespace='blog')),
-    path('api/', include('blog_api.urls', namespace='blog_api'))
+    path('', include(('blog.urls', 'blog'), namespace='blog')),
+    path('api/', include(('blog_api.urls', 'blog_api'), namespace='blog_api'))
 ]
