3ab153d37d63f5425e4323cfa114d5bea0332ae7
Viktor Mishyn <viktormishyn@gmail.com>
2021-05-08 23:20:26 +0300

    Mockup connectionExample in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class connectionExample extends React.Component {
+  componentDidMount() {
+    const apiUrl = "http://127.0.0.1:8000/api/";
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }
+  render() {
+    return <div>Example connection</div>;
+  }
 }
 
-export default App;
+export default connectionExample;
