27e8c3514c99c07007cb9a46f5bf275202656df9
Viktor Mishyn <viktormishyn@gmail.com>
2021-05-09 13:10:51 +0300

    User 'api-auth/' django-rest authentication endpoint

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('', include(('blog.urls', 'blog'), namespace='blog')),
     path('api/', include(('blog_api.urls', 'blog_api'), namespace='blog_api'))
 ]
