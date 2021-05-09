5b3ae56d8dcd5796c6c267ab6521e6aa77eb3f89
Viktor Mishyn <viktormishyn@gmail.com>
2021-05-09 23:42:46 +0300

    Install and configure djangorestframework-simplejwt

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -131,13 +131,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        # 'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.AllowAny',
         # equivalent to 'rest_framework.permissions.AnonymousUser'
         # set by default
         # 'rest_framework.permissions.IsAuthenticated',
         # 'rest_framework.permissions.IsAdminUser',
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    ]
+        # 'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
 
 CORS_ALLOWED_ORIGINS = [
