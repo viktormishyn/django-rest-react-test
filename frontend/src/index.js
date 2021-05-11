9aa7982b52b11e9b78dae7cea0a26bb8593daf35
Viktor Mishyn <viktormishyn@gmail.com>
2021-05-11 13:32:59 +0300

    Add Register component to Header

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import App from "./App";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Register from "./components/Register";
 
 const routing = (
   <Router>
@@ -12,6 +13,7 @@ const routing = (
       <Header />
       <Switch>
         <Route exact path="/" component={App} />
+        <Route path="/register" component={Register} />
       </Switch>
       <Footer />
     </React.StrictMode>
