4e31e55d8e3aca86846b836cf5e1cecb95428404
Viktor Mishyn <viktormishyn@gmail.com>
2021-05-11 12:32:50 +0300

    Register 'api/user/register/' endpoint

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('', include(('blog.urls', 'blog'), namespace='blog')),
-    path('api/', include(('blog_api.urls', 'blog_api'), namespace='blog_api')),
+    path('api/', include('blog_api.urls', namespace='blog_api')),
+    path('api/user/', include('users.urls', namespace='users')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
