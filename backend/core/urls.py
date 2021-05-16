47aa6305d8795c964016bfb7be89462445154ecb
Viktor Mishyn <viktormishyn@gmail.com>
2021-05-16 11:45:22 +0300

    Install pyyaml, uritemplate; Create 'schema/' endpoint

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -4,6 +4,7 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,4 +14,9 @@ urlpatterns = [
     path('api/user/', include('users.urls', namespace='users')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('schema/', get_schema_view(
+        title="BlogAPI",
+        description="API for the BlogAPI",
+        version="1.0.0"
+    ), name='openapi-schema'),
 ]
