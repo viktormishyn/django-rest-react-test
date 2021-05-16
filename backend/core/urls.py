8ceca3640f1205ecfa594847e18199e0ad9aec5e
Viktor Mishyn <viktormishyn@gmail.com>
2021-05-16 12:02:54 +0300

    Refactor core.urls.py (I wanted to change the order of 'schema/', but it didn't change)

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -7,13 +7,13 @@ from rest_framework_simplejwt.views import (
 from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('admin/', admin.site.urls),
     path('', include(('blog.urls', 'blog'), namespace='blog')),
     path('api/', include('blog_api.urls', namespace='blog_api')),
     path('api/user/', include('users.urls', namespace='users')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('schema/', get_schema_view(
         title="BlogAPI",
         description="API for the BlogAPI",
