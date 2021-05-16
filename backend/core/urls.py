08c22e312dbd114ffd226dfd0f54fadeef15efaf
Viktor Mishyn <viktormishyn@gmail.com>
2021-05-16 12:25:22 +0300

    Install and configure and API documentation interface

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -5,6 +5,7 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
@@ -14,6 +15,7 @@ urlpatterns = [
     path('', include(('blog.urls', 'blog'), namespace='blog')),
     path('api/', include('blog_api.urls', namespace='blog_api')),
     path('api/user/', include('users.urls', namespace='users')),
+    path('docs/', include_docs_urls(title='BlogAPI')),
     path('schema/', get_schema_view(
         title="BlogAPI",
         description="API for the BlogAPI",
